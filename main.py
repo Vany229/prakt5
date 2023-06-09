3a12252d4bad42ddabda8c79298d72dcfa638324
Иван <ivanzagulin@yandex.ru>
2023-06-09 03:32:28 +0300

    Начало цикла, который будет проходить по каждому элементу списка. Проверка, является ли текущий элемент положительным. Если элемент положительный, то добавляем его к сумме положительных элементов и увеличиваем количество положительных элементов на 1. Если элемент не положительный, то проверяем, является ли он отрицательным. Если элемент отрицательный, то умножаем его на значение переменной произведения отрицательных элементов. Это необходимо для того, чтобы найти произведение всех отрицательных элементов.

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ array = [2, -3, 5, 7, -4, 0, 8, -1, 6]
 sum_positive = 0
 count_positive = 0
 product_negative = 1
+for element in array:
+    if element > 0:
+        sum_positive += element
+        count_positive += 1
+    elif element < 0:
+        product_negative *= element
